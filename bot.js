1d720321ea2870e8058021d55263c5187d00096a
BoooooooooooDy.! <42409047+Booooody@users.noreply.github.com>
2019-01-19 15:32:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,31 +27,11 @@ client.user.setGame(`Txeeeee,, :))`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-client.on('message', message => {//new msg event
-if(!message.channel.guild) return;
-  if(message.content.startsWith(prefix + 'Rainbow')) {//to create the rainbow role
-      let role = message.guild.roles.find('name', 'Rainbow Quick.')
-    if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
-  //start of create role 
-  if(!role){
-    rainbow =  message.guild.createRole({
-   name: "Rainbow Quick.",//the role will create name
-   color: "#000000",//the default color
-   permissions:[]//the permissions
- //end of create role
-})
-
-}
-message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
-}})
-
-client.on('ready', () => {//new ready event
-  setInterval(function(){
-      client.guilds.forEach(g => {
-                  var role = g.roles.find('name', 'Rainbow Quick.');//rainbow role name
-                  if (role) {
-                      role.edit({color : "RANDOM"});
-                  };
-      });
-  }, 5000);//the rainbow time
-})
+client.on("ready", () => {
+    setInterval(function(){
+        client.guilds.get("505146524192210944").roles.find("name", "' Colors ,").edit({ //Ranibow Code 
+            color : "RANDOM"
+        });
+    }, 1000)
+});
+client.login(process.env.BOT_TOKEN);
