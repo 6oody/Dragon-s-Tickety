f2360b605609d9f84cbd2d21a5b928c9a6f8cd96
BoooooooooooDy.! <42409047+Booooody@users.noreply.github.com>
2019-01-22 15:50:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -128,4 +128,38 @@ channel.guild.owner.send(`<@!${channelremover.id}>
  channelr[channelremover.id].deleted = 0;
   },Otime)
   });
+const developers = ["429217182140858380","348530664108195851","518040402910117908"]
+const adminprefix = "FF";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'pl')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Now Playig   ${argresult} :white_check_mark: '**`)
+  } else 
+     if (message.content === (adminprefix + "leaveserver")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Now Watching   ${argresult} :white_check_mark: '**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'li')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Now Listening   ${argresult} :white_check_mark: '**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/vult");
+      message.channel.send(`**Now Streaming   ${argresult} :white_check_mark: '**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult} :white_check_mark: '** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
 client.login(process.env.BOT_TOKEN);
